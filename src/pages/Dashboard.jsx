334719ff4356d6468caf9cf889a7876ebaf15564
Bekir Geriş <48486272+BekirGeris@users.noreply.github.com>
2022-03-20 02:58:47 +0300

    ee

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Card, Image } from 'semantic-ui-react'
 import EvaluationModels from '../services/EvaluationModels';
 
 export default function Dashboard() {
@@ -12,37 +11,6 @@ export default function Dashboard() {
     })
 
   return (
-    <div className='dashboard'>
-        <Card.Group>
-        {
-                        evaluationModels.map(evaluation => (
-                                            <Card>
-                                                <Card.Content>
-                                                    <Image
-                                                    floated='right'
-                                                    size='mini'
-                                                    src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
-                                                    />
-                                                    <Card.Header>{evaluation.evaluationModelName}</Card.Header>
-                                                    <Card.Meta>Friends of Elliot</Card.Meta>
-                                                    <Card.Description>
-                                                    Steve wants to add you to the group <strong>best friends</strong>
-                                                    </Card.Description>
-                                                </Card.Content>
-                                                <Card.Content extra>
-                                                    <div className='ui two buttons'>
-                                                    <Button basic color='green'>
-                                                        Approve
-                                                    </Button>
-                                                    <Button basic color='red'>
-                                                        Decline
-                                                    </Button>
-                                                    </div>
-                                                </Card.Content>
-                                                </Card>
-                        ))
-                    }
-  </Card.Group>
-    </div>
+    <div>Dashboard</div>
   )
 }
