f455612db0b31d8e7db808bbf1ae53e465e9455a
Bekir Geriş <48486272+BekirGeris@users.noreply.github.com>
2022-06-05 17:45:11 +0300

    link değiştirildi

--- src/pages/SignedIn.jsx
+++ src/pages/SignedIn.jsx
@@ -22,7 +22,7 @@ export default function SignedIn({signOut, goParamaterPanel}) {
                 <Icon size='large' name='user'/>
                 <Dropdown pointing="top left" text={userName}>
                     <Dropdown.Menu>
-                        <Dropdown.Item text="Nasıl Kullanırım?" onClick={() => {window.open("https://www.youtube.com/watch?v=xoP7xNL8jIw&ab_channel=help4ma")}} icon="info"/>
+                        <Dropdown.Item text="Nasıl Kullanırım?" onClick={() => {window.open("https://youtu.be/mgo3VSZIm4Y")}} icon="info"/>
                         <Dropdown.Item onClick={goParamaterPanel} text="Add Parameter" icon="add"/>
                         <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out"/>
                     </Dropdown.Menu>
